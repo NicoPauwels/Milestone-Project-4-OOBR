65aa26fcd469588c0bf0c1911aa711e8e753f691
NicoPauwels <nico_pauwels@hotmail.com>
2021-08-27 20:34:14 +0200

    hide and show side nav

---
+++ static/js/corejs.js
@@ -0,0 +1,11 @@
+// sidenav
+
+function openNav() {
+    document.getElementById("mysidenav").style.width = "250px";
+    document.getElementById("sidenavoverlay").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementById("sidenavoverlay").style.display = "none";
+  }
