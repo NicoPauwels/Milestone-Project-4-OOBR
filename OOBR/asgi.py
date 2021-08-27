db0e0efadc0684b8666bfe31294239c77b5df685
NicoPauwels <nico_pauwels@hotmail.com>
2021-08-27 20:32:18 +0200

    changed app name

--- OOBR/asgi.py
+++ OOBR/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OOBR.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Oobr.settings')
 
 application = get_asgi_application()
