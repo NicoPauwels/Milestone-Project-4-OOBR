580159c8ef6317a91896a01bbda4547a4de58c21
NicoPauwels <nico_pauwels@hotmail.com>
2021-09-30 08:59:24 +0200

    added admin classes

--- business/admin.py
+++ business/admin.py
@@ -1,26 +1,53 @@
 from django.contrib import admin
-from business.models import Business
+from business.models import Business, BusinessType, BusinessCategory
 
 # Register your models here.
 
 class BusinessAdmin(admin.ModelAdmin):
+    
     list_display = (
-        'name',
-        'image',
-        'owner_firstname',
-        'owner_lastname', 
-        'address_1',
-        'address_2 ',
-        'postal_code',
+        'display_name',
+        'business_type',
+        'business_category',
+        'banner_image',
+        'business_name', 
+        'contact_firstname', 
+        'contact_lastname',
+        'address_1', 
+        'address_2', 
+        'postal_code', 
         'city', 
-        'contact_number',
-        'VAT_number',
+        'contact_number', 
+        'VAT_number', 
         'bankaccount_number',
-        'rating',
     )
 
     ordering = (
-        'name',
+        'business_name',
     )
 
 admin.site.register(Business, BusinessAdmin)
+
+
+class BusinessTypeAdmin(admin.ModelAdmin):
+   
+   list_display = (
+        'business_type',
+    )
+
+admin.site.register(BusinessType, BusinessTypeAdmin)
+
+
+class BusinessCategoryAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'business_type',
+        'business_category',
+    )
+
+    ordering = (
+        'business_type',
+        'business_category',
+    )
+
+admin.site.register(BusinessCategory, BusinessCategoryAdmin)
