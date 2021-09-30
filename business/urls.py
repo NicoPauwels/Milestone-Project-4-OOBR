b9ff9f7bf25c0123384b4c517fc0bfe996eb3532
NicoPauwels <nico_pauwels@hotmail.com>
2021-09-30 09:00:36 +0200

    added config business url

--- business/urls.py
+++ business/urls.py
@@ -1,6 +1,8 @@
+from business.forms import BusinessDetail, BusinessHeading, BusinessMenu, BusinessMenuItems, BusinessTables
 from django.urls import path
-from business import views
+from business.views import BusinessConfigWizard
+from business.forms import BusinessHeading, BusinessDetail, BusinessTables, BusinessMenu, BusinessMenuItems
 
 urlpatterns = [
-    path('add/', views.add_business, name='add_business')
+    path('configure/', BusinessConfigWizard.as_view([BusinessHeading, BusinessDetail, BusinessTables, BusinessMenu, BusinessMenuItems]), name='configure_business'),
 ]
